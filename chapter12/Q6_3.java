0468698f03250f02b97ad02db8c8f8766d1a9560
epata <hklee0724@gmail.com>
2023-05-02 06:38:58 +0900

    추가 작업

--- chapter12/Q6_3.java
+++ chapter12/Q6_3.java
@@ -1,7 +1,7 @@
 package chapter12;
 import java.util.Scanner;
 
-public class Q6_2 {
+public class Q6_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("문자열을 입력하세요: ");
@@ -9,7 +9,8 @@ public class Q6_2 {
         System.out.print("제거할 문자를 입력하세요: ");
         String target = scanner.nextLine();
 
-        StringBuffer buffer = new StringBuffer(input);
+//      단순한 문자 조작인 경우 StringBuilder를 사용하자.
+        StringBuilder buffer = new StringBuilder(input);
         int index;
         while ((index = buffer.indexOf(target)) != -1) {
             buffer.deleteCharAt(index);
